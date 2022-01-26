7070d44432c0e560c915f83ed4adad24fa5abfa8
Timothy Phillips <Tphill180@west-mec.local>
2022-01-25 18:15:53 -0700

    Removed console.log.

--- gradient.js
+++ gradient.js
@@ -203,11 +203,15 @@ class Gradient {
 			if (!color.includes("hsl")) {
 				return this.colorKeyMap[color];
 			} else {
-				const hsl = /(hsl\((?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-9][0-9]|3[0-5][0-9]|360),(?:\s|)(?:[0-9]|[1-9][0-9]|100)%),(?:\s|)(?:[0-9]|[1-9][0-9]|100)%\)/gim;
+				const hsl =
+					/(hsl\((?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-9][0-9]|3[0-5][0-9]|360),(?:\s|)(?:[0-9]|[1-9][0-9]|100)%),(?:\s|)(?:[0-9]|[1-9][0-9]|100)%\)/gim;
 				let vals;
 				if (color.match(hsl)) {
-					vals = color.replace("hsl(", "").replace(")", "").split(", ");	
-					
+					vals = color
+						.replace("hsl(", "")
+						.replace(")", "")
+						.split(", ");
+
 					let h = +vals[0];
 					let s = +vals[1].replace("%", "");
 					let l = +vals[2].replace("%", "");
@@ -216,27 +220,39 @@ class Gradient {
 					l /= 100;
 
 					let c = (1 - Math.abs(2 * l - 1)) * s;
-					let x = c * (1 - Math.abs((h / 60) % 2 - 1));
-					let m = l - c/2;
-					
+					let x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+					let m = l - c / 2;
+
 					let r = 0;
 					let g = 0;
 					let b = 0;
 
 					if (0 <= h && h < 60) {
-						r = c; g = x; b = 0;  
+						r = c;
+						g = x;
+						b = 0;
 					} else if (60 <= h && h < 120) {
-						r = x; g = c; b = 0;
+						r = x;
+						g = c;
+						b = 0;
 					} else if (120 <= h && h < 180) {
-						r = 0; g = c; b = x;
+						r = 0;
+						g = c;
+						b = x;
 					} else if (180 <= h && h < 240) {
-						r = 0; g = x; b = c;
+						r = 0;
+						g = x;
+						b = c;
 					} else if (240 <= h && h < 300) {
-						r = x; g = 0; b = c;
+						r = x;
+						g = 0;
+						b = c;
 					} else if (300 <= h && h < 360) {
-						r = c; g = 0; b = x;
+						r = c;
+						g = 0;
+						b = x;
 					}
-				
+
 					r = Math.round((r + m) * 255);
 					g = Math.round((g + m) * 255);
 					b = Math.round((b + m) * 255);
@@ -252,7 +268,7 @@ class Gradient {
 			});
 		}
 
-		throw new Error(`[gradient.js] Given color "${color}" has bad syntax.`)
+		throw new Error(`[gradient.js] Given color "${color}" has bad syntax.`);
 	}
 
 	/**
@@ -277,8 +293,6 @@ class Gradient {
 		for (let i = 0; i < colors.length; i++) {
 			colors[i] = this.convertToRGBA(colors[i]);
 		}
-
-		console.log(colors)
 
 		const gradient = [];
 
